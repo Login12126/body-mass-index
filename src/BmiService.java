22287e8d3ef92c330286352a74708384afb047c3
loginov <pandamiksms@gmail.com>
2026-03-28 08:17:41 +0300

    Commitnew

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    public int calculate (double height, double weight) {
+    public int calculate(double height, double weight) {
         double index = weight / (height * height);
         return (int) index;
     }
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
         double height = 1.87;
-        int weight= 98;
-        int bodyMassIndex = service.calculate(height, weight );
+        int weight = 98;
+        int bodyMassIndex = service.calculate(height, weight);
         System.out.println(bodyMassIndex);
     }
 }
